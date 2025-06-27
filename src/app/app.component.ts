84f2e2246c5ddce3970ceb0d8e487dc3eaac0e94
Timofey_Babisashvili <timofey.babisashvili@gmail.com>
2025-06-27 15:46:41 +0300

    Add DataService and update app configuration
    - Implement DataService for fetching front page data from the API.
    - Update app configuration to include HttpClient provider.
    - Modify app.component.ts to utilize DataService for data retrieval.
    - Update .gitignore to exclude environment files.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { DataService } from './data.service';
 
 @Component({
   selector: 'app-root',
@@ -7,6 +8,14 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'jupiter-frontpage';
+
+  constructor(private dataService: DataService) {}
+
+  ngOnInit() {
+    this.dataService.fetchFrontPageData().subscribe(result => {
+      console.log(result);
+    });
+  }
 }
